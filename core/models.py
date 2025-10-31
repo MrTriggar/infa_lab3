b6f25001e2d92ac042ef43d126d124ff83d83325
MrTrigger <georgsid512@gmail.com>
2025-10-31 07:10:35 +0300

    еще ОРМ

--- core/models.py
+++ core/models.py
@@ -4,31 +4,6 @@ from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase, Mapped, mappe
 from datetime import datetime
 from core.database import *
 
-class Product(Base):
-    __tablename__ = "products"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    product_name: Mapped[str] = mapped_column(String(200))
-    price: Mapped[int]
-    categorie_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
-    seller_id: Mapped[int] = mapped_column(ForeignKey("sellers.id"))
-
-    categories: Mapped[List["Categories"]] = relationship(back_populates="products")
-    seller: Mapped["Seller"] = relationship(back_populates="products")
-
-
-class Order(Base):
-    __tablename__ = "orders"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    buyer_id: Mapped[int] = mapped_column(ForeignKey("buyers.id"))
-    order_price: Mapped[int]
-    count_of_products: Mapped[int]
-    created_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
-
-    buyer: Mapped["Buyer"] = relationship(back_populates="orders")
-    products: Mapped[List["Product"]] = relationship(back_populates="orders")
-
 
 class Buyer(Base):
     __tablename__ = "buyers"
@@ -41,7 +16,6 @@ class Buyer(Base):
         server_default=text("TIMEZONE('utc', now())"),
         onupdate=datetime.utcnow
     )
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
 
     orders: Mapped[List["Order"]] = relationship(back_populates="buyers")
 
@@ -67,3 +41,29 @@ class Categories(Base):
     categorie_name: Mapped[str] = mapped_column(String(200))
 
     products: Mapped[List["Product"]] = relationship(back_populates="categories")
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_name: Mapped[str] = mapped_column(String(200))
+    price: Mapped[int]
+    categorie_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"))
+    seller_id: Mapped[int] = mapped_column(ForeignKey("sellers.id", ondelete="CASCADE"))
+
+    categories: Mapped[List["Categories"]] = relationship(back_populates="products")
+    seller: Mapped["Seller"] = relationship(back_populates="products")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    buyer_id: Mapped[int] = mapped_column(ForeignKey("buyers.id", ondelete="CASCADE"))
+    order_price: Mapped[int]
+    count_of_products: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+
+    buyer: Mapped["Buyer"] = relationship(back_populates="orders")
+    products: Mapped[List["Product"]] = relationship(back_populates="orders")
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from core.ORM import ORM
-from core.settings import config
-from core.models import *
-
-database = ORM(config["DATABASE_URL"])
 
 if __name__ == "__main__":
-    # database.delete_table()
-    # database.create_table()
-    database.insert_buyer_data()
-    database.insert_product_data()
-    database.insert_categorie_data()
-    database.insert_order_data()
+    ORM.delete_table()
+    ORM.create_table()
+    ORM.insert_buyer_data()
+    ORM.insert_product_data()
+    ORM.insert_categorie_data()
+    ORM.insert_order_data()
